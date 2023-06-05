4f0391de96afa69112a21c35a9757e60902b5454
OOGD <oogd17@gmail.com>
2023-06-05 12:38:24 -0600

    Agregando media Querys a los estilos

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Icono } from "../UI";
+import { Detalle, Icono, Btn, Saldo, Box } from "../UI";
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
@@ -18,7 +18,7 @@ const Account = () => {
   };
 
   return (
-    <div className="box">
+    <Box>
       <h2>Cuenta</h2>
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponible
@@ -26,19 +26,19 @@ const Account = () => {
           <Icono src={dinero} alt="Icono de saldo" />
         </span>
         {toggleState ? (
-          <div className="saldo">
-            <span className="detalle">$</span> 8,621.50
-          </div>
+          <Saldo>
+            <Detalle>$</Detalle> 8,621.50
+          </Saldo>
         ) : null}
       </div>
 
-      <button className="btn" onClick={toggleHandler}>
+      <Btn onClick={toggleHandler}>
         <IconoMargin
           src={toggleState ? privado : ojo}
           alt="Privacidad de saldo"
         />
-      </button>
-    </div>
+      </Btn>
+    </Box>
   );
 };
 
