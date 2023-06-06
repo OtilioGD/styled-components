41949636eeac5bdd51e3cd694ff1d4e962dc8366
OOGD <oogd17@gmail.com>
2023-06-06 16:43:46 -0600

    Agregand iconos a la lista de detalles

---
+++ src/ImageFilter.js
@@ -0,0 +1,20 @@
+import alimentacion from './assets/images/alimentacion.svg';
+import salud from './assets/images/salud.svg';
+import transporte from './assets/images/transporte.svg';
+import utilidades from './assets/images/utilidades.svg';
+import otros from './assets/images/otros.svg';
+
+import { Icono } from './Components/UI';
+
+
+export default (type) => {
+
+    const Images = {
+        Restaurante: <Icono src={alimentacion} alt='Restaurante' />,
+        Salud: <Icono src={salud} alt='Salud' />,
+        Transporte: <Icono src={transporte} alt='Transporte' />,
+        Utilidades: <Icono src={utilidades} alt='Utilidades' />,
+        default: <Icono src={otros} alt='Restaurante' />,
+    }
+    return Images[type] || Images["default"];
+}
