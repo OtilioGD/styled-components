edabdce27549866d7a5594c730671b3eb75382e2
OOGD <oogd17@gmail.com>
2023-06-07 11:06:41 -0600

    creando tema claro - oscuro

--- src/Components/UI/Variables.js
+++ src/Components/UI/Variables.js
@@ -1,4 +1,12 @@
 
 export const colorPrimario = '#41D3BE';
+
 export const fondoClaro = 'F1F1F1';
+export const contenidoClaro = 'white';
 export const textoFondoClaro = 'grey';
+
+export const fondoOscuro = '#363537';
+export const contenidoOscuro = '#5C5B5E';
+export const textoFondoOscuro = '#FAFAFA';
+
+export const iconosOscuro = 'invert(95%) sepia(100%) saturate(0%) hue-rotate(214deg) brightness(106%) contrast(102%)';
