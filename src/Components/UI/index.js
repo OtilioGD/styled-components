76f8ac5334e88378171f3fe25082df5e49051faa
OOGD <oogd17@gmail.com>
2023-06-07 11:30:42 -0600

    Creando btn theme on/off para cambiar el tema

--- src/Components/UI/index.js
+++ src/Components/UI/index.js
@@ -43,3 +43,12 @@ export const Detalle = styled.span`
     color: #41d3be;
     font-size: 24px;
 `;
+
+export const btnTema = styled.div`
+  position: absolute;
+  top: 4vh;
+  right: 20px;
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
+`;
